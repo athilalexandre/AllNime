928787b9737a1ba1d1dbc84e764d4dee8cd71ab4
Athila Alexandre <athilalexandre@gmail.com>
2025-06-08 23:30:49 -0300

    chore: Update Tailwind CSS to version 3.4.17 and refactor PostCSS configuration
    - Downgraded Tailwind CSS from version 4.1.8 to 3.4.17 in package.json and package-lock.json.
    - Refactored postcss.config.js to use an object format for plugins.
    - Added new tailwind.config.cjs file for Tailwind CSS configuration.

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,7 @@
 // postcss.config.js
 export default {
-  plugins: [
-    'tailwindcss', // Tailwind CSS v3 direto
-    'autoprefixer',
-  ],
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 };
